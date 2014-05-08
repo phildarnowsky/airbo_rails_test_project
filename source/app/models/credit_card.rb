5ac8381cc0839128a14f104ceec30b3363c9a41a
Krishna Srihari <krishna.srihari@gmail.com>
2014-05-08 02:15:00 -0700

    charge view

--- source/app/models/credit_card.rb
+++ source/app/models/credit_card.rb
@@ -1,3 +1,4 @@
 class CreditCard < ActiveRecord::Base
   belongs_to :customer
+  delegate :name, to: :customer
 end
--- source/spec/requests/charges_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'charges', js: true do
-  
-  describe 'index' do
-    visit root_path
-  end
-  
-end
