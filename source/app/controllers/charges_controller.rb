02c3a70d9e5dd126babf32bbc7f29d8d52d51fa9
Krishna Srihari <krishna.srihari@gmail.com>
2014-05-08 00:41:06 -0700

    request spec

--- source/app/controllers/charges_controller.rb
+++ source/app/controllers/charges_controller.rb
@@ -1,4 +1,7 @@
 class ChargesController < ApplicationController
-  def index       
+  def index   
+     @charges = CreditCard.where(paid: true, refunded: false)
+     @disputes = CreditCard.where(paid: true, refunded: true)
+     @fails = CreditCard.where(paid: false, refunded: false)   
   end
 end
---
+++ source/spec/requests/charges_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'charges', js: true do
+  
+  describe 'index' do
+    visit root_path
+  end
+  
+end
