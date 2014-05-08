bab743281c147808ac048087fa485878e1d46059
Krishna Srihari <krishna.srihari@gmail.com>
2014-05-07 22:49:40 -0700

    credit card migration

--- source/db/migrate/20140508045004_create_credit_cards.rb
+++ source/db/migrate/20140508045004_create_credit_cards.rb
@@ -1,10 +1,10 @@
 class CreateCreditCards < ActiveRecord::Migration
   def change
     create_table :credit_cards do |t|
-      t.boolean :paid
+      t.boolean :paid, default: false
       t.integer :amount
       t.string :currency
-      t.boolean :refunded
+      t.boolean :refunded, default: false
       t.integer :customer_id
 
       t.timestamps
